8057b82d7793280b566d1e512f5a593ad710e4f4
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-03-01 13:40:21 +0600

    add Phone Details function and Phone details display

--- JS/app.js
+++ JS/app.js
@@ -7,11 +7,8 @@ const searchPhone = () => {
     let searchText = searchField.value;
     // console.log(searchText);
 
-if(searchText == ""){
+if(searchText == "" || searchText<=0){
     error.innerText='Please Search Phone Name,try again';
-}
-else if(searchText<=0){
-    error.innerText='Please Search phone name,try again';
 }
 else{ 
     error.innerText = '';
@@ -27,24 +24,85 @@ else{
         // console.log(phones);
         
     const  searchResult = document.getElementById('search-result');
-    // console.log(searchResult);
     searchResult.textContent = '';
-    phones.forEach(phone => {
-        console.log(phone);
-      const div = document.createElement('div')
-      div.classList.add('col');
-      div.innerHTML = `
-      <div class="card p-3 text-center" style="width: 18rem;">
-      <img  src="${phone.image}" class="card-img-top w-50 ms-5" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">${phone.phone_name}</h5>
-        <p class="card-text">${phone.brand}</p>
-        <button class="btn btn-primary">Details</button>
-      </div>
-    </div>
-      `
-      searchResult.appendChild(div);
-
-    });
-
+    if(phones==""){
+        alert('Show no result found, Please search Phone name , try again')
+        
+        // error.innerText='Please try again';   
     }
+        // console.log(phone);
+     else{
+        phones?.forEach(phone => {
+            const div = document.createElement('div')
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card p-3 text-center" style="width: 18rem;">
+            <img  src="${phone.image}" class="card-img-top w-50 ms-5" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${phone.phone_name}</h5>
+              <p class="card-text">${phone.brand}</p>
+              <button onclick="phoneDetails('${phone.slug}')" class="btn btn-primary">Details</button>
+            </div>
+          </div>
+            `
+            searchResult.appendChild(div);
+        });
+     }
+   
+    }
+
+    const phoneDetails = id => {
+        const url = `https://openapi.programming-hero.com/api/phone/${id}`
+        // console.log(url);
+        fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetails(data.data))
+    }
+
+    const displayPhoneDetails = phoneId => {
+        console.log(phoneId);
+     const phoneDetailsId = document.getElementById('phone-details');
+       phoneDetailsId.textContent = ''; 
+       const div = document.createElement('div')
+       div.classList.add('card');
+       div.innerHTML = `
+       
+       <div style="width: 18rem; class="card text-center mb-5 p-5" class="phonecard">
+       <img src="${phoneId.image}" class="card-img-top defaultcard" alt="...">
+       <div class="card-body ">
+         <h4>PhoneName:${phoneId.name} </h4>
+         <h5 class="card-text">${phoneId.brand}</h5>
+         <h6>ReleaseDate:${phoneId.releaseDate}</h6>
+         <div class="d-flex justify-content-between align-content-center">
+         <div>
+         <h5>Main Feture: 
+         <ul class="phone-feature">
+         <li>Storage: ${phoneId.mainFeatures.storage}</li>
+         <li>DisplaySize: ${phoneId.mainFeatures.displaySize}</li>
+         <li>ChipSet: ${phoneId.mainFeatures.chipSet}</li>
+         <li>Memory: ${phoneId.mainFeatures.memory}</li>
+         </ul>
+         </h5>
+         </div>
+        <div>
+        <h5>Others: 
+        <ul class="phone-feature">
+        <li>Storage: ${phoneId.others.WLAN}</li>
+        <li>DisplaySize: ${phoneId.others.Bluetooth}</li>
+        <li>ChipSet: ${phoneId.others.GPS}</li>
+        <li>Memory: ${phoneId.others.NFC}</li>
+        <li>Memory: ${phoneId.others.Radio}</li>
+        </ul>
+        </h5>
+        </div>
+         </div>
+         
+       </div>
+     </div>
+
+       `
+       phoneDetailsId.appendChild(div)
+    }
+
+
+   
